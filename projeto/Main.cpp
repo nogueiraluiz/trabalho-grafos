a243dcbedf6116a0a1d944399c66478a59457ec7
Luiz Fernando Nogueira <mnogueira.luizf@gmail.com>
2024-04-26 21:46:48 -0300

    Obtendo argumentos para a main

---
+++ projeto/Main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+// #include "headers/Aresta.hpp"
+// #include "headers/Grafo.hpp"
+// #include "headers/Aresta.hpp"
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    char *arquivoInput = argv[1];
+    char *arquivoOutput = argv[2];
+    char *direcionado = argv[3];
+    char *ponderadoArestas = argv[4];
+    char *ponderadoVertices = argv[5];
+    cout << arquivoInput << endl;
+    cout << arquivoOutput << endl;
+    cout << direcionado << endl;
+    cout << ponderadoArestas << endl;
+    cout << ponderadoVertices << endl;
+    return 0;
+}
