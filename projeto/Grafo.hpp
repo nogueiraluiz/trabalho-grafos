8d761258aae6d3fd12daf50d6b77b21de074d39b
Luiz Fernando Nogueira <128930394+nogueiraluiz@users.noreply.github.com>
2024-08-22 19:37:27 -0300

    Update branch profundidade (#44)
    * Incluindo funcionalidade de fecho transitivo indireto
    
    * Adaptando para permitir salvamento de fecho transitivo direto como grafo
    
    * Documentando método de análise por excentricidade
    
    * Adaptando fecho transitivo indireto como retorno em grafo
    
    * Adicionando atributo 'id_origem' a 'Aresta'
    
    * Adicionando atributos necessários
    
    * Fazendo as alterações necessárias para implementação própria de lista encadeada
    
    * Update floyd (#39)
    
    * Corrigindo métodos de remoção de elementos
    
    * Merge master -> excentricidade
    
    Atualizando para bugfix
    
    * Merge master -> excentricidade
    
    * Incluindo análise de excentricidade
    
    * Documentando e organizando
    
    * Incluindo funcionalidade de fecho transitivo indireto
    
    * Adaptando para permitir salvamento de fecho transitivo direto como grafo
    
    * Documentando método de análise por excentricidade
    
    * Adaptando fecho transitivo indireto como retorno em grafo
    
    * Adicionando atributo 'id_origem' a 'Aresta'
    
    * Adicionando atributos necessários
    
    * Fazendo as alterações necessárias para implementação própria de lista encadeada
    
    * Incluindo reconstrução do caminho no algoritmo de Floyd-Warshall
    
    Também usando std::numeric_limits ao invés de limits.h para padronizar com o algoritmo de Dijkstra implementado pelo Igor
    
    * Update branch articulacao (#41)
    
    * Incluindo funcionalidade de fecho transitivo indireto
    
    * Adaptando para permitir salvamento de fecho transitivo direto como grafo
    
    * Documentando método de análise por excentricidade
    
    * Adaptando fecho transitivo indireto como retorno em grafo
    
    * Adicionando atributo 'id_origem' a 'Aresta'
    
    * Adicionando atributos necessários
    
    * Fazendo as alterações necessárias para implementação própria de lista encadeada
    
    * Update floyd (#39)
    
    * Corrigindo métodos de remoção de elementos
    
    * Merge master -> excentricidade
    
    Atualizando para bugfix
    
    * Merge master -> excentricidade
    
    * Incluindo análise de excentricidade
    
    * Documentando e organizando
    
    * Incluindo funcionalidade de fecho transitivo indireto
    
    * Adaptando para permitir salvamento de fecho transitivo direto como grafo
    
    * Documentando método de análise por excentricidade
    
    * Adaptando fecho transitivo indireto como retorno em grafo
    
    * Adicionando atributo 'id_origem' a 'Aresta'
    
    * Adicionando atributos necessários
    
    * Fazendo as alterações necessárias para implementação própria de lista encadeada
    
    * Incluindo reconstrução do caminho no algoritmo de Floyd-Warshall
    
    Também usando std::numeric_limits ao invés de limits.h para padronizar com o algoritmo de Dijkstra implementado pelo Igor
    
    * Incluindo identificação conjunto dos vértices de articulação de um grafo
    
    Incluindo também a identificação das componentes conexas de um grafo, pelo fato de, na implementação utilizada para os vértices de articulação, foi necessário garantir que a busca em profundidade cobrisse todas as componentes

--- projeto/Grafo.hpp
+++ projeto/Grafo.hpp
@@ -22,11 +22,12 @@ public:
     void adicionaVertice(int idVertice, int peso = 0);
     void adicionaAresta(int idVerticeU, int idVerticeV, int peso = 0);
     void removeVertice(int idVertice);
-    void removeAresta(int idVerticeU, int idVerticeV);
-    void liberaMemoriaArestas(std::list<Aresta *> &arestas);
-    void fechoTransitivoDireto(int idVertice);
-    void caminhoMinimoFloyd(int idVerticeU, int idVerticeV);
+    bool removeAresta(int idVerticeU, int idVerticeV);
+    Grafo *fechoTransitivoDireto(int idVertice);
+    Grafo *fechoTransitivoIndireto(int idVertice);
+    Grafo *caminhoMinimoFloyd(int idVerticeU, int idVerticeV);
     void analiseExcentricidade();
+    Grafo *verticesDeArticulacao();
     void caminhamentoProfundidade(int idVerticeInicio);
 
 private:
@@ -37,14 +38,24 @@ private:
     Vertice *getVertice(int id);
     void adicionaAdjacencias(int idA, int idB, int peso = 0);
     bool existeAresta(int idVerticeU, int idVerticeV);
-    void auxFechoDireto(Vertice *vertice, std::set<int> &fecho);
+    void auxFechoDireto(Vertice* vertice, std::set<int>& fecho, Grafo *grafoFecho);
     int encontraIndiceVertice(int id);
     void inicializaMatrizDistancias(std::vector<std::vector<int>> &distancias, int ordem);
     void atualizaMatrizDistancias(std::vector<std::vector<int>> &distancias, int ordem, int indice);
     std::vector<std::vector<int>> getMatrizDistancias();
+    void inicializaMatrizesFloyd(std::vector<std::vector<int>>& distancias, std::vector<std::vector<int>>& proximos, int ordem);
+    void atualizaMatrizesFloyd(std::vector<std::vector<int>>& distancias, std::vector<std::vector<int>>& proximos, int ordem, int indice);
+    bool auxFechoIndireto(std::set<int>& fecho, std::vector<Vertice*>& naoUtilizados, Grafo *grafoFecho);
+    void liberaMemoriaArestas(Aresta* arestas);
+    void buscaProfundidadeComponente(Vertice *v, std::map<Vertice*, bool>& usados, std::set<Vertice*>& componente);
+    std::set<Vertice*> buscaComponente(Vertice *v, std::map<Vertice*, bool>& usados);
+    std::set<std::set<Vertice*>> getComponentesConexas();
+    void buscaProfundidadeArticulacoes(std::set<int>& articulacoes, Vertice *v, Vertice* pai, std::map<int, int>& tempoEntrada, std::map<int, int>& minimo, std::map<int, bool> visitado, int& cronometro);
+    std::set<int> encontraArticulacoesComponente(Vertice *v, std::set<Vertice*> componente);
     int getExcentricidade(const std::vector<int> &distanciasVertice);
     void caminhaProfundidade(Vertice *u, std::map<Vertice *, int> &entrada, std::map<Vertice *, int> &saida, std::map<Vertice *, int> &cor, int &tempo);
     void preencheMapaInicialProfundidade(std::map<Vertice *, int> &mapa);
+  
 };
 
 #endif
