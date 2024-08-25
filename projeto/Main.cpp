bba92726cd7168a644f81aab6aef22be7399835c
Luiz Fernando Nogueira <mnogueira.luizf@gmail.com>
2024-08-25 19:10:55 -0300

    Corrigindo erros de merge e erros de compilação provenientes do menu

--- projeto/Main.cpp
+++ projeto/Main.cpp
@@ -19,31 +19,37 @@ void opcoes(Grafo* g, std::ofstream& output)
         std::cout << "9 - Conjunto dos vértices de articulação\n";
         std::cout << "10 - Sair" << std::endl;
         std::cin >> opcao;
-        switch (opcao) {
-            case 1: 
+        switch (opcao)
+        {
+        case 1:
+        {
             int idVert;
             std::cout << "Escolha id do vértice que deseja usar a funcionalidade Fecho transitivo direto:";
             std::cin >> idVert;
-            Grafo* fecho = g->fechoTransitivoDireto(idVert);
-            if(fecho != nullptr){
+            Grafo *fecho = g->fechoTransitivoDireto(idVert);
+            if (fecho != nullptr)
+            {
                 int r;
-                while(r!=1 || r!=2){
+                while (r != 1 || r != 2)
+                {
                     std::cout << "Deseja salvar no arquivo? Sim-1 Não-2";
                     std::cin >> r;
                     std::cout << std::endl;
                 }
-                if(r==1){
+                if (r == 1)
+                {
                     g->print(output);
                 }
-                delete fecho;
             }
-
-            // TODO: implementar os tratamentos especiais de cada opção (obtenção do parâmetro que o usuário deseja)
-            default:
-                std::cout << "Opção inválida\n";
-                break;
+            delete fecho;
+            break;
         }
-        // espaço para implementação da opção de salvar ou não o resultado no arquivo 'output'
+        default:
+        {
+            std::cout << "Opção inválida\n";
+            break;
+        }
+        }
     }
 }
 
